ac77be4e9fc4457b454b1a2c00d4605e24f14ee2
Travis Nischuk <trakout@gmail.com>
2014-12-07 12:27:52 -0500

    cleanup

--- pages/js/main.js
+++ pages/js/main.js
@@ -1,14 +1,153 @@
 
+function runSocket() {
+	$('#conversationSection').hide();
+
+	var _userObject;
+
+	/*************************************************************************************
+
+	* BUTTON HANDLER RELATED STUFF
+
+	*************************************************************************************/
+
+	$('#onlineUsers').on('click','li.onlineUser',function() {    
+		requestToChatWithUser($(this).data("user-id"));
+	});
+
+	$('#chatRequests').on('click','li.chatRequest',function() {    
+		console.log($(this).data("user-id"));
+		acceptChatRequest($(this).data("user-id"));
+	});
+
+	/*************************************************************************************
+
+	* VIDEO RELATED STUFF
+
+	*************************************************************************************/
+
+	function startOpenTok(roomObject) {
+		console.log(roomObject);
+		var apiKey = "45102212";
+		var session = OT.initSession(apiKey, roomObject.id);
+
+		session.on("streamCreated", function(event) {
+			console.log('OpenTok stream created');
+			session.subscribe(event.stream);
+		});
+
+		session.connect(roomObject.token, function(error) {
+			console.log('OpenTok session connected');
+			var publisher = OT.initPublisher();
+			session.publish(publisher);
+		});
+	}
+
+	/*************************************************************************************
+
+	* SOCKET RELATED STUFF
+
+	*************************************************************************************/
+	var socket = io();
+	$('#conversationForm').submit(function(){
+
+		// send to the server the message
+		socket.emit('chatMessage', $('#messageInput').val());
+			$('#messageInput').val('');
+		return false;
+	});
+
+	$('#changeNameForm').submit(function(){
+
+		// send to the server the message
+		socket.emit('changeName', $('#usernameInput').val());
+		return false;
+	});
+
+	// recieved message
+	socket.on('chatMessage', function(msg){
+		$('#conversationTranscript').append($('<li>').text(msg));
+	});
+
+	socket.on('startChat', function (roomObject) {
+		console.log('starting chat');
+		$('#conversationSection').show();
+
+		startOpenTok(roomObject);
+	});
+
+	socket.on('receiveChatRequest', function (userObject) {
+		console.log("recieved: "+ userObject);
+		if ($('#chatRequests').find('[data-user-id="' + userObject.id + '"]').length == 0) {
+			$('#chatRequests').append('<li class="chatRequest" data-user-id="'+userObject.id+'">'+userObject.name+'</li>');
+		}
+	});
+
+	socket.on('updatedUserObject', function (userObject) {
+		_userObject = userObject;
+		$('#usernameInput').val(userObject.name);
+	});
+
+	// recieved a list of people online
+	// NOTE: this will probably be a list of objects later, right now it is strings
+	socket.on('listOfUsersOnline', function(userArray) {
+
+		// refresh the list of online users
+		$('#onlineUsers').empty();
+		var keys = Object.keys(userArray);
+		for (var i=0; i<keys.length; ++i) {
+			var userObject = userArray[keys[i]];
+			if (userObject.id != _userObject.id) {
+				$('#onlineUsers').append('<li class="onlineUser" data-user-id="'+userObject.id+'">'+userObject.name+'</li>');
+			}
+		}
+	});
+
+	socket.on('receivedSessionDescription', function(sessionDescription) {
+		console.log('received session description: ' + sessionDescription);
+	});
+
+	socket.on('receivedCandidateEvent', function(candidateEvent) {
+		console.log('got candidate event');
+		var candidate = new RTCIceCandidate({
+			sdpMLineIndex: candidateEvent.label,
+			candidate: candidateEvent.candidate
+		});
+	});
+
+	function requestToChatWithUser(userId) {
+		socket.emit('sendChatRequest', userId);
+	}
+
+	function acceptChatRequest(userId) {
+		socket.emit('acceptChatRequest', userId);
+	}
+
+	// function sendCandidateEvent(event) {
+	// 	socket.emit('sendCandidateEvent', {
+	// 		type: 'candidate',
+	// 		label: event.candidate.sdpMLineIndex,
+	// 		id: event.candidate.sdpMid,
+	// 		candidate: event.candidate.candidate
+	// 	});
+	// }
+
+	// function sendSessionDescription(sessionDescription) {
+	// 	console.log('sending session description');
+	// 	socket.emit('sendSessionDescription', sessionDescription);
+	// }
+} // runSocket
+
 
 function sendString(val) {
 	console.log('this was sent: ' + val);
+
 
 	
 	// run socket to other peer
 }
 
 // keyboard shortcuts
-$('.text-submit textarea').bind('keyup', function(e) {
+$('.text-submit textarea').bind('keydown', function(e) {
 	var code = e.keyCode || e.which;
 	if (code == 13 && e.shiftKey == false && ($('.text-submit textarea').val().length > 0)) { 
 
@@ -27,7 +166,7 @@ $('.text-submit textarea').bind('keyup', function(e) {
 			$('textarea').val('');
 			// user submitting blank data, disallowed
 		}
-	}
+	} 
 });
 
 function autoResize(e) {
@@ -83,4 +222,5 @@ function checkDom() {
 
 $(document).ready(function() {
 	checkDom();
+	runSocket();
 });
